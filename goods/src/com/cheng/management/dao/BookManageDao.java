0e2ca1b1c56351604e6ad260d38b86d4d7d9ce94
cheng <chengz.9705@outlook.com>
2017-07-17 18:29:28 +0800

    2017-7-16 goods 1.1.3
    管理员模块设计，已完成：
    3.图书管理：
    ①各种查询：与前台相同②添加图书③上传图片④修改图书 与添加图书相似，也使用动态下拉列表⑤删除图书
    需要删除图书对应图片，再删除图书⑥页面中使用动态下拉列表显示2级分类，当指定1级分类后，2级分类下拉列表中动态显示该1级分类下所有2级分类名称。

---
+++ goods/src/com/cheng/management/dao/BookManageDao.java
@@ -0,0 +1,83 @@
+package com.cheng.management.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cheng.dao.BookMapper;
+import com.cheng.domain.Book;
+import com.cheng.domain.BookExample;
+
+@Repository
+public class BookManageDao implements BookMapper {
+	
+	@Autowired
+	private BookMapper bookMapper;
+	
+	@Override
+	public List<Book> selectByCidForAllBook(String cid) {
+		return this.bookMapper.selectByCidForAllBook(cid);
+	}
+
+	@Override
+	public int countByExample(BookExample example) {
+		return this.bookMapper.countByExample(example);
+	}
+
+	@Override
+	public List<Book> selectForBooks(Book book) {
+		return this.bookMapper.selectForBooks(book);
+	}
+
+	@Override
+	public int deleteByExample(BookExample example) {
+		return this.bookMapper.deleteByExample(example);
+	}
+
+	@Override
+	public int deleteByPrimaryKey(String bid) {
+		return this.bookMapper.deleteByPrimaryKey(bid);
+	}
+
+	@Override
+	public int insert(Book record) {
+		return this.bookMapper.insert(record);
+	}
+
+	@Override
+	public int insertSelective(Book record) {
+		return this.bookMapper.insertSelective(record);
+	}
+
+	@Override
+	public List<Book> selectByExample(BookExample example) {
+		return this.bookMapper.selectByExample(example);
+	}
+
+	@Override
+	public Book selectByPrimaryKey(String bid) {
+		return this.bookMapper.selectByPrimaryKey(bid);
+	}
+
+	@Override
+	public int updateByExampleSelective(Book record, BookExample example) {
+		return this.bookMapper.updateByExampleSelective(record, example);
+	}
+
+	@Override
+	public int updateByExample(Book record, BookExample example) {
+		return this.bookMapper.updateByExample(record, example);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(Book record) {
+		return this.bookMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int updateByPrimaryKey(Book record) {
+		return this.bookMapper.updateByPrimaryKey(record);
+	}
+
+}
